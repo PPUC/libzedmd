a68ea0999831e989d3b3ca705b3ad0f895a6dc2c
Jason Millard <jsm174@gmail.com>
2024-01-06 08:48:23 -0500

    misc: patch libserialport name to libserialport64

--- platforms/win/x64/external.sh
+++ platforms/win/x64/external.sh
@@ -20,7 +20,8 @@ curl -sL https://github.com/sigrokproject/libserialport/archive/${LIBSERIALPORT_
 unzip libserialport.zip
 cd libserialport-$LIBSERIALPORT_SHA
 cp libserialport.h ../../third-party/include
+patch libserialport.vcxproj < ../../platforms/win/x64/libserialport.patch
 msbuild.exe libserialport.sln -p:Configuration=Release -p:Platform=x64
-cp x64/Release/libserialport.lib ../../third-party/build-libs/win/x64
-cp x64/Release/libserialport.dll ../../third-party/runtime-libs/win/x64
+cp x64/Release/libserialport64.lib ../../third-party/build-libs/win/x64
+cp x64/Release/libserialport64.dll ../../third-party/runtime-libs/win/x64
 cd ..
