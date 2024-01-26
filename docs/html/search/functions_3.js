952645836917948c7c29e8b87746f51a8f69a725
Markus Kalkbrenner <git@kalki.de>
2024-01-26 13:02:15 +0100

    updated docs

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['getdefaultpalette_0',['GetDefaultPalette',['../class_ze_d_m_d.html#a5fef8bdf5f31dd475219e6560f514d0b',1,'ZeDMD']]]
+  ['getdefaultpalette_0',['GetDefaultPalette',['../class_ze_d_m_d.html#a5fef8bdf5f31dd475219e6560f514d0b',1,'ZeDMD']]],
+  ['getheight_1',['GetHeight',['../class_ze_d_m_d.html#a06d6c538c25ed80d7da9cf0e498f7de4',1,'ZeDMD']]],
+  ['getwidth_2',['GetWidth',['../class_ze_d_m_d.html#ab65ebb83795ac5bf5c8872c87f3d9568',1,'ZeDMD']]]
 ];
--- docs/html/search/functions_7.js
+++ docs/html/search/functions_7.js
@@ -4,5 +4,6 @@ var searchData=
   ['rendergray2_1',['RenderGray2',['../class_ze_d_m_d.html#a82cf2fb5e909d745c3b8e6f4ab612c31',1,'ZeDMD']]],
   ['rendergray4_2',['RenderGray4',['../class_ze_d_m_d.html#a7855bccff91c35545b82ce4f3f5f9a45',1,'ZeDMD']]],
   ['renderrgb24_3',['RenderRgb24',['../class_ze_d_m_d.html#a05b36262d0de83d5450c1a00fd64df71',1,'ZeDMD']]],
-  ['renderrgb24encodedas565_4',['RenderRgb24EncodedAs565',['../class_ze_d_m_d.html#ad06f69cffd2c48ee6ad31f2e65b2150b',1,'ZeDMD']]]
+  ['renderrgb24encodedas565_4',['RenderRgb24EncodedAs565',['../class_ze_d_m_d.html#ad06f69cffd2c48ee6ad31f2e65b2150b',1,'ZeDMD']]],
+  ['renderrgb565_5',['RenderRgb565',['../class_ze_d_m_d.html#ae167684f041fa33a317a5f99335c630c',1,'ZeDMD']]]
 ];
