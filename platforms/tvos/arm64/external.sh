84462578f90b196b782809c076c7277258976689
Markus Kalkbrenner <git@kalki.de>
2024-04-02 09:32:43 +0200

    Libframeutil (#20)
    use libframeutil

--- platforms/tvos/arm64/external.sh
+++ platforms/tvos/arm64/external.sh
@@ -2,10 +2,25 @@
 
 set -e
 
-rm -rf mkdir
-mkdir mkdir
-cd mkdir
+LIBFRAMEUTIL_SHA=4da694f9518a01fcf13105c33c2ae1a396ec0aea
+
+NUM_PROCS=$(sysctl -n hw.ncpu)
+
+echo "Building libraries..."
+echo "  LIBFRAMEUTIL_SHA: ${LIBFRAMEUTIL_SHA}"
+echo "  NUM_PROCS: ${NUM_PROCS}"
+echo ""
+
+rm -rf external
+mkdir external
+cd external
 
 #
-# tvos has no external dependencies
+# copy libframeutil
 #
+
+curl -sL https://github.com/ppuc/libframeutil/archive/${LIBFRAMEUTIL_SHA}.zip -o libframeutil.zip
+unzip libframeutil.zip
+cd libframeutil-$LIBFRAMEUTIL_SHA
+cp include/* ../../third-party/include
+cd ..
--- platforms/win/x64/external.sh
+++ platforms/win/x64/external.sh
@@ -3,9 +3,11 @@
 set -e
 
 LIBSERIALPORT_SHA=fd20b0fc5a34cd7f776e4af6c763f59041de223b
+LIBFRAMEUTIL_SHA=4da694f9518a01fcf13105c33c2ae1a396ec0aea
 
 echo "Building libraries..."
 echo "  LIBSERIALPORT_SHA: ${LIBSERIALPORT_SHA}"
+echo "  LIBFRAMEUTIL_SHA: ${LIBFRAMEUTIL_SHA}"
 echo ""
 
 rm -rf external
@@ -24,4 +26,14 @@ patch libserialport.vcxproj < ../../platforms/win/x64/libserialport.patch
 msbuild.exe libserialport.sln -p:Configuration=Release -p:Platform=x64
 cp x64/Release/libserialport64.lib ../../third-party/build-libs/win/x64
 cp x64/Release/libserialport64.dll ../../third-party/runtime-libs/win/x64
+cd ..
+
+#
+# copy libframeutil
+#
+
+curl -sL https://github.com/ppuc/libframeutil/archive/${LIBFRAMEUTIL_SHA}.zip -o libframeutil.zip
+unzip libframeutil.zip
+cd libframeutil-$LIBFRAMEUTIL_SHA
+cp include/* ../../third-party/include
 cd ..
