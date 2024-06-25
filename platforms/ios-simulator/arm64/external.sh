55224e9d1a790ca8abfdae7357c015c80609fd7a
Jason Millard <jsm174@gmail.com>
2024-06-25 07:39:07 -0400

    misc: add support for ios simulator platform

---
+++ platforms/ios-simulator/arm64/external.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+LIBFRAMEUTIL_SHA=30048ca23d41ca0a8f7d5ab75d3f646a19a90182
+
+NUM_PROCS=$(sysctl -n hw.ncpu)
+
+echo "Building libraries..."
+echo "  LIBFRAMEUTIL_SHA: ${LIBFRAMEUTIL_SHA}"
+echo "  NUM_PROCS: ${NUM_PROCS}"
+echo ""
+
+rm -rf external
+mkdir external
+cd external
+
+#
+# copy libframeutil
+#
+
+curl -sL https://github.com/ppuc/libframeutil/archive/${LIBFRAMEUTIL_SHA}.zip -o libframeutil.zip
+unzip libframeutil.zip
+cd libframeutil-$LIBFRAMEUTIL_SHA
+cp include/* ../../third-party/include
+cd ..
