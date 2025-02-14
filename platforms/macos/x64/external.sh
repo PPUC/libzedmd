4487023aef5019f3b0ce6814834603fa758abcf1
Jason Millard <jsm174@gmail.com>
2025-02-13 20:08:57 -0500

    misc: shorten externals, add linux arm64 runners

--- platforms/macos/x64/external.sh
+++ platforms/macos/x64/external.sh
@@ -29,9 +29,10 @@ cd external
 # build cargs and copy to external
 #
 
-curl -sL https://github.com/likle/cargs/archive/${CARGS_SHA}.zip -o cargs.zip
-unzip cargs.zip
-cd cargs-${CARGS_SHA}
+curl -sL https://github.com/likle/cargs/archive/${CARGS_SHA}.tar.gz -o cargs-${CARGS_SHA}.tar.gz
+tar xzf cargs-${CARGS_SHA}.tar.gz
+mv cargs-${CARGS_SHA} cargs
+cd cargs
 cmake \
    -DBUILD_SHARED_LIBS=ON \
    -DCMAKE_OSX_ARCHITECTURES=x86_64 \
@@ -47,9 +48,10 @@ cd ..
 # build libserialport and copy to platform/arch
 #
 
-curl -sL https://github.com/sigrokproject/libserialport/archive/${LIBSERIALPORT_SHA}.zip -o libserialport.zip
-unzip libserialport.zip
-cd libserialport-$LIBSERIALPORT_SHA
+curl -sL https://github.com/sigrokproject/libserialport/archive/${LIBSERIALPORT_SHA}.tar.gz -o libserialport-${LIBSERIALPORT_SHA}.tar.gz
+tar xzf libserialport-${LIBSERIALPORT_SHA}.tar.gz
+mv libserialport-${LIBSERIALPORT_SHA} libserialport
+cd libserialport
 cp libserialport.h ../../third-party/include
 ./autogen.sh
 ./configure --host=x86_64-apple-darwin CFLAGS="-arch x86_64" LDFLAGS="-Wl,-install_name,@rpath/libserialport.dylib"
@@ -62,9 +64,10 @@ cd ..
 # copy libframeutil
 #
 
-curl -sL https://github.com/ppuc/libframeutil/archive/${LIBFRAMEUTIL_SHA}.zip -o libframeutil.zip
-unzip libframeutil.zip
-cd libframeutil-$LIBFRAMEUTIL_SHA
+curl -sL https://github.com/ppuc/libframeutil/archive/${LIBFRAMEUTIL_SHA}.tar.gz -o libframeutil-${LIBFRAMEUTIL_SHA}.tar.gz
+tar xzf libframeutil-${LIBFRAMEUTIL_SHA}.tar.gz
+mv libframeutil-${LIBFRAMEUTIL_SHA} libframeutil
+cd libframeutil
 cp include/* ../../third-party/include
 cd ..
 
@@ -72,9 +75,10 @@ cd ..
 # build sockpp and copy to external
 #
 
-curl -sL https://github.com/fpagliughi/sockpp/archive/${SOCKPP_SHA}.zip -o sockpp.zip
-unzip sockpp.zip
-cd sockpp-$SOCKPP_SHA
+curl -sL https://github.com/fpagliughi/sockpp/archive/${SOCKPP_SHA}.tar.gz -o sockpp-${SOCKPP_SHA}.tar.gz
+tar xzf sockpp-${SOCKPP_SHA}.tar.gz
+mv sockpp-${SOCKPP_SHA} sockpp
+cd sockpp
 cmake \
    -DSOCKPP_BUILD_SHARED=ON \
    -DSOCKPP_BUILD_STATIC=OFF \
--- platforms/win/x86/external.sh
+++ platforms/win/x86/external.sh
@@ -26,9 +26,10 @@ cd external
 # build cargs and copy to external
 #
 
-curl -sL https://github.com/likle/cargs/archive/${CARGS_SHA}.zip -o cargs.zip
-unzip cargs.zip
-cd cargs-${CARGS_SHA}
+curl -sL https://github.com/likle/cargs/archive/${CARGS_SHA}.tar.gz -o cargs-${CARGS_SHA}.tar.gz
+tar xzf cargs-${CARGS_SHA}.tar.gz
+mv cargs-${CARGS_SHA} cargs
+cd cargs
 cmake \
    -G "Visual Studio 17 2022" \
    -DBUILD_SHARED_LIBS=ON \
@@ -44,9 +45,10 @@ cd ..
 # build libserialport and copy to platform/arch
 #
 
-curl -sL https://github.com/sigrokproject/libserialport/archive/${LIBSERIALPORT_SHA}.zip -o libserialport.zip
-unzip libserialport.zip
-cd libserialport-$LIBSERIALPORT_SHA
+curl -sL https://github.com/sigrokproject/libserialport/archive/${LIBSERIALPORT_SHA}.tar.gz -o libserialport-${LIBSERIALPORT_SHA}.tar.gz
+tar xzf libserialport-${LIBSERIALPORT_SHA}.tar.gz
+mv libserialport-${LIBSERIALPORT_SHA} libserialport
+cd libserialport
 cp libserialport.h ../../third-party/include
 msbuild.exe libserialport.sln -p:Configuration=Release -p:Platform=x86
 cp Release/libserialport.lib ../../third-party/build-libs/win/x86
@@ -57,9 +59,10 @@ cd ..
 # copy libframeutil
 #
 
-curl -sL https://github.com/ppuc/libframeutil/archive/${LIBFRAMEUTIL_SHA}.zip -o libframeutil.zip
-unzip libframeutil.zip
-cd libframeutil-$LIBFRAMEUTIL_SHA
+curl -sL https://github.com/ppuc/libframeutil/archive/${LIBFRAMEUTIL_SHA}.tar.gz -o libframeutil-${LIBFRAMEUTIL_SHA}.tar.gz
+tar xzf libframeutil-${LIBFRAMEUTIL_SHA}.tar.gz
+mv libframeutil-${LIBFRAMEUTIL_SHA} libframeutil
+cd libframeutil
 cp include/* ../../third-party/include
 cd ..
 
@@ -67,9 +70,10 @@ cd ..
 # build sockpp and copy to external
 #
 
-curl -sL https://github.com/fpagliughi/sockpp/archive/${SOCKPP_SHA}.zip -o sockpp.zip
-unzip sockpp.zip
-cd sockpp-$SOCKPP_SHA
+curl -sL https://github.com/fpagliughi/sockpp/archive/${SOCKPP_SHA}.tar.gz -o sockpp-${SOCKPP_SHA}.tar.gz
+tar xzf sockpp-${SOCKPP_SHA}.tar.gz
+mv sockpp-${SOCKPP_SHA} sockpp
+cd sockpp
 cmake \
    -G "Visual Studio 17 2022" \
    -A Win32 \
